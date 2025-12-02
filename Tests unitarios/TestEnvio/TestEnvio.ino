35899eefefedfbec9bf567c37b749550214cdbe1
rogeralonsovilella-gif <roger.alonso.vilella@estudiantat.upc.edu>
2025-12-02 22:35:43 +0100

    Versión 3

--- Tests unitarios/TestEnvio/TestEnvio.ino
+++ Tests unitarios/TestEnvio/TestEnvio.ino
@@ -1,18 +1,18 @@
-// Test unitario enviar
-void setup() {
-  #include <SoftwareSerial.h>
-SoftwareSerial mySerial(10, 11); // RX, TX (en este Arduino)
-void setup() {
-  delay (5000)
-  mySerial.begin(9600);
-  // Inicialización del sensor
-  dht.begin();
-  Serial.println("Compravacion de comunicación: ");
-  mySerial.print("Me recibes??")
+#include <SoftwareSerial.h>
 
+SoftwareSerial mySerial(10, 11);  // RX, TX
+int i = 0;
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+
+  Serial.println("Arduino envio listo)");
 }
 
 void loop() {
- 
-
+  mySerial.print("¿Me recibes?");
+  mySerial.print(i);
+  Serial.println("Mensaje enviado: ¿Me recibes?");
+  delay(2000);
+  i = i+1;
 }
